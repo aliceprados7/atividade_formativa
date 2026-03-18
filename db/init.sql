72b18aa0e83aab2ab33fe6791678a435d4115689
Alice <alice.prados07@gmail.com>
2026-03-18 20:50:55 -0300

    criação do banco de dados

---
+++ db/init.sql
@@ -0,0 +1,12 @@
+create database monitoramento;
+SHOW monitoramento;
+use monitoramento;
+
+create table registros (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    data_hora DATETIME 
+    temperatura DECIMAL (5, 2) NOT NULL,
+    umidade DECIMAL (5, 2) NOT NULL,
+    origem_dado VARCHAR (50) NOT NULL,
+    data_insercao DATETIME DEFAULT CURRENT_TIMESTAMP
+);
