12808da40d9d8a98e66d6e81f46081111deb7c7c
JOÃO VICTOR DA SILVA NEVES SOARES <joao.soares81@senaisp.edu.br>
2026-03-25 19:48:06 -0300

    ajustes finais pt2 e documentação

--- app/app.py
+++ app/app.py
@@ -1,3 +1,4 @@
+# importações
 import threading
 import requests
 import random
@@ -7,65 +8,83 @@ from flask_cors import CORS
 from banco import conectar
 from config import SECRET_KEY, FLASK_DEBUG
 
+# chave ThingSpeak
 API_KEY = "ZXCK40DOE70O40AW"
 
-
+# função que simula dados coletados a cada 15 segundos
+# função de escrita no ThingSpeak
 def writeThingSpeak():
-    """Loop infinito que envia dados aleatórios a cada 15 segundos"""
     while True:
+        # dados simulados de forma randomica
         temperatura = round(random.uniform(20, 30), 2)
         umidade = round(random.uniform(50, 70), 2)
-        
+        # url de escrita da plataforma
         url = f"https://api.thingspeak.com/update?api_key={API_KEY}&field1={temperatura}&field2={umidade}"
         try:
             reposta = requests.get(url)
-            print(f"[Thread Envio] Dados enviados: T={temperatura}, U={umidade} | Status: {reposta.text}")
+            print(f"Dados enviados: T={temperatura}, U={umidade} | Status: {reposta.text}")
         except Exception as e:
-            print(f"[Thread Envio] Erro ao enviar: {e}")
-            
+            print(f"Erro ao enviar: {e}")
+        # tempo de espera
         time.sleep(15)
 
+# função que lê os dados do ThingSpeak
 def readThingSpeak():
+    # id do canal, número de resultados esperados e url da plataforma
     channel_id = 3305425
     num_results = 10
-    url = f"https://api.thingspeak.com/channels/3305425/feeds.json?results=10"
+    url = "https://api.thingspeak.com/channels/{}/feeds.json?results={}".format(channel_id, num_results)
 
     response = requests.get(url)
     dados_processados = []
-
+    # caso busca bem sucedidda
     if response.status_code == 200:
         data = response.json()
+        # a plataforma envia dados nomeados de feeeds
+        # "feeds": [
+        # {
+        #   "created_at": "2026-03-25T22:37:35Z",
+        #   "entry_id": 429,
+        #   "field1": "21.64",
+        #   "field2": "62.61"
+        # },
         feeds = data['feeds']
+
+        # abrindo conexão com o banco
         conexao = conectar()
         cursor = conexao.cursor()
-
+        # percorrendo os feeds enviados
         for registro in feeds:
             temp = registro['field1']
             umid = registro['field2']
+            # adequando a data conforme o banco de dados
             dt_hr = registro['created_at'].replace('T', ' ').replace('Z', '')
 
             try:
+                # inserindo valores no banco
                 cursor.execute(
                     "INSERT INTO registros (data_hora, temperatura, umidade, origem_dado) VALUES (%s, %s, %s, %s)",
                     (dt_hr, temp, umid, "ThingSpeak")
                 )
+                # criando um json de dados recebidos
                 dados_processados.append({
                     "data_hora": dt_hr, "temperatura": temp, "umidade": umid, "origem": "ThingSpeak"
                 })
             except Exception as e:
                 print(f"Erro no banco: {e}")
-
+        # fechando conexão e enviando os dados
         conexao.commit()
         cursor.close()
         conexao.close()
         return dados_processados
     return []
 
-
+# configurações de aplicações
 app = Flask(__name__)
 CORS(app)
 app.secret_key = SECRET_KEY
 
+# url responsável por buscar dados no banco e disponibilizar no localhost:5050
 @app.get("/")
 def listar_dados():
     conexao = conectar()
@@ -80,6 +99,7 @@ def listar_dados():
     conexao.close()
     return jsonify({"dados": dados})
 
+# url responsável por buscar dados do thingspeak e disponibilizar no localhost:5050/dados
 @app.get("/dados")
 def listar_dados_ts():
     dados_lidos = readThingSpeak()
@@ -89,6 +109,8 @@ def listar_dados_ts():
 
 
 if __name__ == "__main__":
+    # usando threading para rodar a aplicação e o while que envia dados para a plataforma
+    # necessário pois são dois serviços em paralelo
     tarefa_envio = threading.Thread(target=writeThingSpeak, daemon=True)
     tarefa_envio.start()
 
--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,19 +1,29 @@
+const botaoConexao = document.getElementById("conexao")
+let input = document.getElementById("ip");
+// funcão responsável port buscar os dados
 async function carregarDados() {
-    try {
-        const resposta = await fetch("http://localhost:5000/"); 
+    let ipServidor = input.value;
+
+    if(ipServidor){
+        try {
+        // acessa a url rodando localmente para puxar os dados do banco de dados
+        const resposta = await fetch(`http://${ipServidor}:5000/`); 
         const json = await resposta.json();
-        const listaRegistros = json.dados; 
+        // variavel que recebe os dados
+        const listaRegistros = json.dados;
+        // variavel que recebe os ultimos dados enviados 
         const ultimoRegistro = listaRegistros.at(-1);
 
+        // caso exista dados
         if (listaRegistros.length > 0) {
-         
+            // altera os valores do front-end
             document.getElementById("temp").innerText = ultimoRegistro.temperatura + " °C";
             document.getElementById("umid").innerText = ultimoRegistro.umidade + " %";
-            document.getElementById("qtd").innerText = listaRegistros.length;
+            document.getElementById("qtd").innerText = listaRegistros.length; // recebe o tamanho do json, ou seja, a quantidade de dados enviados
 
             const tabelaBd = document.getElementById("tabela-bd");
             tabelaBd.innerHTML = "";
-
+            // cria novos elementos dentro da tabela
             listaRegistros.forEach(item => {
                 tabelaBd.innerHTML += `
                     <tr>
@@ -25,14 +35,14 @@ async function carregarDados() {
                 `;
             });
         }
-
-        const respostaTS = await fetch("http://localhost:5000/dados");
+        // recebe os dados recebidos pela função readThingSpeak, cujo lê os dados da nuvem
+        const respostaTS = await fetch(`http://${ipServidor}:5000/dados`);
         const jsonTS = await respostaTS.json();
-        
+        // controle para escrita na tabea
         if (jsonTS.status === "sucesso") {
             const tabelaApi = document.getElementById("tabela-api");
             tabelaApi.innerHTML = "";
-            
+            // atualiza a respectiva tabela
             jsonTS.dados.forEach((item, index) => {
                 tabelaApi.innerHTML += `
                     <tr>
@@ -46,8 +56,12 @@ async function carregarDados() {
         }
 
     } catch (erro) {
-        console.error("Erro ao carregar dados:", erro);
+        alert("Erro ao carregar dados. Verifique a conexão e a porta conectada", erro);
     }
+    }
+
+
+    
 }
 
 carregarDados();
